0575d8d8de961579e8f89733d5d29a9c6ec333ff
prsprshr@gmail.com <123573626+ParasParashar@users.noreply.github.com>
2024-02-28 00:37:19 +0530

    add the message skeleton loading

--- app/(messages)/messages/page.js
+++ app/(messages)/messages/page.js
@@ -1,32 +1,36 @@
+import MessageHomeSkeleton from "@/components/SkeletonLoaders/MessageHomeSkeleton";
 import MobileSidebar from "@/components/messages/MobileSidebar";
 import SearchUsers from "@/components/messages/SearchUsers";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Suspense } from "react";
 import { BiSolidMessageDetail } from "react-icons/bi";
 
 const page = () => {
   return (
-    <main className="flex flex-col  items-center  w-full h-full  justify-center gap-2">
-      <section className="relative h-1/2 w-1/2">
-        <Image src={"/chatImage.png"} alt="Chat Image" fill />
-      </section>
-      <div className="text-lg font-mono text-muted-foreground flex items-center">
-        <h4>Select a conversation to start messaging.</h4>
-        <div className="block lg:hidden">
-          <MobileSidebar />
+    <Suspense fallback={<MessageHomeSkeleton />}>
+      <main className="flex flex-col  items-center  w-full h-full  justify-center gap-2">
+        <section className="relative h-1/2 w-1/2">
+          <Image src={"/chatImage.png"} alt="Chat Image" fill />
+        </section>
+        <div className="text-lg font-mono text-muted-foreground flex items-center">
+          <h4>Select a conversation to start messaging.</h4>
+          <div className="block lg:hidden">
+            <MobileSidebar />
+          </div>
         </div>
-      </div>
-      <SearchUsers>
-        <Button
-          variant="secondary"
-          className=" font-bold text-lg font-mono"
-          size="sm"
-        >
-          Create a Chat{" "}
-          <BiSolidMessageDetail size={20} className="text-blue-500" />
-        </Button>
-      </SearchUsers>
-    </main>
+        <SearchUsers>
+          <Button
+            variant="secondary"
+            className=" font-bold text-lg font-mono"
+            size="sm"
+          >
+            Create a Chat{" "}
+            <BiSolidMessageDetail size={20} className="text-blue-500" />
+          </Button>
+        </SearchUsers>
+      </main>
+    </Suspense>
   );
 };
 
--- components/SkeletonLoaders/MessageHomeSkeleton.jsx
+++ components/SkeletonLoaders/MessageHomeSkeleton.jsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Loading() {
+export default function MessageHomeSkeleton() {
   return (
     <div className="flex flex-col p-40  items-center justify-center h-full w-full">
       <Skeleton className=" mx-auto  h-[200px] w-[200px] lg:w-[500px] lg:h-[300px] rounded-lg" />
